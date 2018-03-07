1b822ca4316b6b7721cdec25f496c289ab2476b9
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2018-03-07 16:34:22 +0000

    Added infrastructure for Japanese translations.

---
+++ iOS/sakura/translations/sakura_ja.qml.ts
@@ -0,0 +1,157 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>CampaignPage</name>
+    <message>
+        <location filename="../qml/CampaignPage.qml" line="132"/>
+        <source>STEP:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/CampaignPage.qml" line="219"/>
+        <source>TIME:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/CampaignPage.qml" line="711"/>
+        <source>Game over. Do you want to play again?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/CampaignPage.qml" line="888"/>
+        <source>Congratulations, you completed all available levels! Stay tuned for updates with new levels, challenges and more.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/MainPage.qml" line="38"/>
+        <source>TUTORIAL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="71"/>
+        <source>CAMPAIGN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="111"/>
+        <source>TOURNAMENT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="144"/>
+        <source>STORE</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RatingsListPage</name>
+    <message>
+        <location filename="../qml/RatingsListPage.qml" line="40"/>
+        <source>The tournament will end in:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/RatingsListPage.qml" line="236"/>
+        <source>TOP SCORE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/RatingsListPage.qml" line="355"/>
+        <source>Your nickname:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SingleGamePage</name>
+    <message>
+        <location filename="../qml/SingleGamePage.qml" line="123"/>
+        <location filename="../qml/SingleGamePage.qml" line="739"/>
+        <source>SCORE:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SingleGamePage.qml" line="194"/>
+        <source>STEP:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SingleGamePage.qml" line="281"/>
+        <source>TIME:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SingleGamePage.qml" line="788"/>
+        <source>Game over. Do you want to play again?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SingleLevelsPage</name>
+    <message>
+        <location filename="../qml/SingleLevelsPage.qml" line="355"/>
+        <source>Your nickname:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StorePage</name>
+    <message>
+        <location filename="../qml/StorePage.qml" line="16"/>
+        <location filename="../qml/StorePage.qml" line="19"/>
+        <source>BUY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/StorePage.qml" line="865"/>
+        <source>Remove Ads</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/StorePage.qml" line="909"/>
+        <source>Restore purchases</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/StorePage.qml" line="925"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TutorialPage</name>
+    <message>
+        <location filename="../qml/TutorialPage.qml" line="324"/>
+        <source>Excellent!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/TutorialPage.qml" line="417"/>
+        <source>Try to resize playground area with pinch gesture</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/TutorialPage.qml" line="434"/>
+        <source>Try to move playground area with your finger</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/TutorialPage.qml" line="450"/>
+        <source>Double tap on playground area to restore its default position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/TutorialPage.qml" line="466"/>
+        <source>Disassembling puzzle...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/TutorialPage.qml" line="482"/>
+        <source>Tap on highlighted fragments to solve the puzzle</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
