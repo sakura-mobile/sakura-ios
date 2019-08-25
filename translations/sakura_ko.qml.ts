5255113c49b8d1d0507d97353179756c841c0ff7
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-08-25 19:48:44 +0300

    Always add locations to .ts files, regen translations.

--- translations/sakura_ko.qml.ts
+++ translations/sakura_ko.qml.ts
@@ -4,14 +4,17 @@
 <context>
     <name>AdMobConsentDialog</name>
     <message>
+        <location filename="../qml/AdMobConsentDialog.qml" line="30"/>
         <source>We keep this app free by showing ads. Ad network will &lt;a href=&quot;https://policies.google.com/technologies/ads&quot;&gt;collect data and use a unique identifier on your device&lt;/a&gt; to show you ads. &lt;b&gt;Do you allow to use your data to tailor ads for you?&lt;/b&gt;</source>
         <translation>우리는 광고를 게재하여이 응용 프로그램을 무료로 유지합니다. 광고 네트워크는 &lt;a ​​href=&quot;https://policies.google.com/technologies/ads&quot;&gt;데이터를 수집하고 기기에서 고유 식별자를 사용&lt;/a&gt; 하여 광고를 게재합니다. &lt;b&gt;데이터를 사용하여 광고를 맞춤 설정할 수 있습니까?&lt;/b&gt;</translation>
     </message>
     <message>
+        <location filename="../qml/AdMobConsentDialog.qml" line="62"/>
         <source>Yes, show me relevant ads</source>
         <translation>예, 관련 광고 표시</translation>
     </message>
     <message>
+        <location filename="../qml/AdMobConsentDialog.qml" line="99"/>
         <source>No, show me ads that are less relevant</source>
         <translation>아니요, 관련성이 낮은 광고를 게재하십시오</translation>
     </message>
@@ -19,22 +22,27 @@
 <context>
     <name>CampaignPage</name>
     <message>
+        <location filename="../qml/CampaignPage.qml" line="138"/>
         <source>STEP:</source>
         <translation>단계:</translation>
     </message>
     <message>
+        <location filename="../qml/CampaignPage.qml" line="239"/>
         <source>TIME:</source>
         <translation>시각:</translation>
     </message>
     <message>
+        <location filename="../qml/CampaignPage.qml" line="736"/>
         <source>Game over. Do you want to play again?</source>
         <translation>게임 끝. 다시 놀고 싶니?</translation>
     </message>
     <message>
+        <location filename="../qml/CampaignPage.qml" line="845"/>
         <source>Create postcard and share with friends</source>
         <translation>엽서 만들기 및 친구와 공유</translation>
     </message>
     <message>
+        <location filename="../qml/CampaignPage.qml" line="998"/>
         <source>Congratulations, you completed all available levels! Stay tuned for updates with new levels, challenges and more.</source>
         <translation>축하합니다. 사용 가능한 모든 레벨을 완료하셨습니다! 새로운 차원, 도전 과제 등으로 업데이트를 계속 지켜보십시오.</translation>
     </message>
@@ -42,26 +50,32 @@
 <context>
     <name>MainPage</name>
     <message>
+        <location filename="../qml/MainPage.qml" line="39"/>
         <source>TUTORIAL</source>
         <translation>지도 시간</translation>
     </message>
     <message>
+        <location filename="../qml/MainPage.qml" line="72"/>
         <source>CAMPAIGN</source>
         <translation>캠페인</translation>
     </message>
     <message>
+        <location filename="../qml/MainPage.qml" line="108"/>
         <source>TOURNAMENT</source>
         <translation>토너먼트</translation>
     </message>
     <message>
+        <location filename="../qml/MainPage.qml" line="141"/>
         <source>RELAX</source>
         <translation>편하게 하다</translation>
     </message>
     <message>
+        <location filename="../qml/MainPage.qml" line="174"/>
         <source>STORE</source>
         <translation>가게</translation>
     </message>
     <message>
+        <location filename="../qml/MainPage.qml" line="200"/>
         <source>SETTINGS</source>
         <translation>구성</translation>
     </message>
@@ -69,10 +83,12 @@
 <context>
     <name>RatingsListPage</name>
     <message>
+        <location filename="../qml/RatingsListPage.qml" line="41"/>
         <source>The tournament will end in:</source>
         <translation>토너먼트 종료일 :</translation>
     </message>
     <message>
+        <location filename="../qml/RatingsListPage.qml" line="249"/>
         <source>TOP SCORE</source>
         <translation>최고 점수</translation>
     </message>
@@ -80,14 +96,17 @@
 <context>
     <name>RelaxGamePage</name>
     <message>
+        <location filename="../qml/RelaxGamePage.qml" line="137"/>
         <source>STEP:</source>
         <translation>단계:</translation>
     </message>
     <message>
+        <location filename="../qml/RelaxGamePage.qml" line="538"/>
         <source>Create postcard and share with friends</source>
         <translation>엽서 만들기 및 친구와 공유</translation>
     </message>
     <message>
+        <location filename="../qml/RelaxGamePage.qml" line="673"/>
         <source>Congratulations, you completed all available levels! Stay tuned for updates with new levels, challenges and more.</source>
         <translation>축하합니다. 사용 가능한 모든 레벨을 완료하셨습니다! 새로운 차원, 도전 과제 등으로 업데이트를 계속 지켜보십시오.</translation>
     </message>
@@ -95,14 +114,17 @@
 <context>
     <name>SettingsPage</name>
     <message>
+        <location filename="../qml/SettingsPage.qml" line="50"/>
         <source>MUSIC</source>
         <translation>게임 음악</translation>
     </message>
     <message>
+        <location filename="../qml/SettingsPage.qml" line="94"/>
         <source>SOUNDS</source>
         <translation>게임 사운드</translation>
     </message>
     <message>
+        <location filename="../qml/SettingsPage.qml" line="141"/>
         <source>AD PRIVACY SETTINGS</source>
         <translation>광고 개인 정보 설정</translation>
     </message>
@@ -110,18 +132,23 @@
 <context>
     <name>SingleGamePage</name>
     <message>
+        <location filename="../qml/SingleGamePage.qml" line="130"/>
+        <location filename="../qml/SingleGamePage.qml" line="770"/>
         <source>SCORE:</source>
         <translation>점수:</translation>
     </message>
     <message>
+        <location filename="../qml/SingleGamePage.qml" line="210"/>
         <source>STEP:</source>
         <translation>단계:</translation>
     </message>
     <message>
+        <location filename="../qml/SingleGamePage.qml" line="312"/>
         <source>TIME:</source>
         <translation>시각:</translation>
     </message>
     <message>
+        <location filename="../qml/SingleGamePage.qml" line="819"/>
         <source>Game over. Do you want to play again?</source>
         <translation>게임 끝. 다시 놀고 싶니?</translation>
     </message>
@@ -129,14 +156,17 @@
 <context>
     <name>SingleLevelsPage</name>
     <message>
+        <location filename="../qml/SingleLevelsPage.qml" line="192"/>
         <source>Your reward:</source>
         <translation>보상 :</translation>
     </message>
     <message>
+        <location filename="../qml/SingleLevelsPage.qml" line="367"/>
         <source>Your nickname:</source>
         <translation>닉네임 :</translation>
     </message>
     <message>
+        <location filename="../qml/SingleLevelsPage.qml" line="511"/>
         <source>Participate in daily tournaments and get rewards!</source>
         <translation>매일 토너먼트에 참가하고 보상을 받으십시오!</translation>
     </message>
@@ -144,18 +174,23 @@
 <context>
     <name>StorePage</name>
     <message>
+        <location filename="../qml/StorePage.qml" line="18"/>
+        <location filename="../qml/StorePage.qml" line="21"/>
         <source>BUY</source>
         <translation>사다</translation>
     </message>
     <message>
+        <location filename="../qml/StorePage.qml" line="888"/>
         <source>Remove Ads</source>
         <translation>광고를 제거하다</translation>
     </message>
     <message>
+        <location filename="../qml/StorePage.qml" line="938"/>
         <source>Restore purchases</source>
         <translation>구매를 복원</translation>
     </message>
     <message>
+        <location filename="../qml/StorePage.qml" line="955"/>
         <source>OK</source>
         <translation>승인</translation>
     </message>
@@ -163,26 +198,32 @@
 <context>
     <name>TutorialPage</name>
     <message>
+        <location filename="../qml/TutorialPage.qml" line="325"/>
         <source>Excellent!</source>
         <translation>우수한!</translation>
     </message>
     <message>
+        <location filename="../qml/TutorialPage.qml" line="418"/>
         <source>Try to resize playground area with pinch gesture</source>
         <translation>핀치 제스처로 놀이터 영역의 크기를 조정 해보십시오</translation>
     </message>
     <message>
+        <location filename="../qml/TutorialPage.qml" line="437"/>
         <source>Try to move playground area with your finger</source>
         <translation>손가락으로 놀이터를 움직여보십시오</translation>
     </message>
     <message>
+        <location filename="../qml/TutorialPage.qml" line="455"/>
         <source>Double tap on playground area to restore its default position</source>
         <translation>놀이터 영역을 두 번 탭하면 기본 위치로 돌아갑니다</translation>
     </message>
     <message>
+        <location filename="../qml/TutorialPage.qml" line="473"/>
         <source>Disassembling puzzle...</source>
         <translation>퍼즐 분해...</translation>
     </message>
     <message>
+        <location filename="../qml/TutorialPage.qml" line="491"/>
         <source>Tap on highlighted fragments to solve the puzzle</source>
         <translation>강조 표시된 조각을 탭하여 퍼즐을 해결하십시오</translation>
     </message>
@@ -190,6 +231,7 @@
 <context>
     <name>main</name>
     <message>
+        <location filename="../qml/main.qml" line="8"/>
         <source>Sakura</source>
         <translation>사쿠라 퍼즐</translation>
     </message>
