8693ce865418a712a8e22e1ea30322bcd60aae79
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-11-13 17:04:15 +0300

    Style nits: sort files in project and scripts alphabetically.

--- sakura.pro
+++ sakura.pro
@@ -6,26 +6,27 @@ CONFIG += c++11
 
 DEFINES += QT_DEPRECATED_WARNINGS QT_NO_CAST_FROM_ASCII QT_NO_CAST_TO_ASCII
 
-SOURCES += src/main.cpp \
+SOURCES += \
     src/gifcreator.cpp \
+    src/main.cpp \
     src/uuidcreator.cpp
 
 OBJECTIVE_SOURCES += \
-    src/sharehelper.mm \
     src/admobhelper.mm \
-    src/storehelper.mm \
     src/audiohelper.mm \
-    src/reachabilityhelper.mm
+    src/reachabilityhelper.mm \
+    src/sharehelper.mm \
+    src/storehelper.mm
 
 HEADERS += \
-    src/uuidcreator.h \
+    src/admobhelper.h \
+    src/audiohelper.h \
     src/gif.h \
     src/gifcreator.h \
+    src/reachabilityhelper.h \
     src/sharehelper.h \
-    src/admobhelper.h \
     src/storehelper.h \
-    src/audiohelper.h \
-    src/reachabilityhelper.h
+    src/uuidcreator.h
 
 RESOURCES += \
     qml.qrc \
@@ -33,14 +34,14 @@ RESOURCES += \
     translations.qrc
 
 TRANSLATIONS += \
-    translations/sakura_ru.ts \
     translations/sakura_de.ts \
+    translations/sakura_es.ts \
     translations/sakura_fr.ts \
     translations/sakura_it.ts \
-    translations/sakura_es.ts \
-    translations/sakura_zh.ts \
     translations/sakura_ja.ts \
-    translations/sakura_ko.ts
+    translations/sakura_ko.ts \
+    translations/sakura_ru.ts \
+    translations/sakura_zh.ts
 
 # Additional import path used to resolve QML modules in Qt Creator's code model
 QML_IMPORT_PATH =
--- tools/gen_ts.sh
+++ tools/gen_ts.sh
@@ -2,11 +2,11 @@
 
 PATH=$PATH:~/Qt/5.13.0/ios/bin
 
-lupdate -locations absolute ../sakura.pro -ts ../translations/sakura_ru.src.ts
-lupdate -locations absolute ../qml        -ts ../translations/sakura_ru.qml.ts
-
 lupdate -locations absolute ../sakura.pro -ts ../translations/sakura_de.src.ts
 lupdate -locations absolute ../qml        -ts ../translations/sakura_de.qml.ts
+
+lupdate -locations absolute ../sakura.pro -ts ../translations/sakura_es.src.ts
+lupdate -locations absolute ../qml        -ts ../translations/sakura_es.qml.ts
 
 lupdate -locations absolute ../sakura.pro -ts ../translations/sakura_fr.src.ts
 lupdate -locations absolute ../qml        -ts ../translations/sakura_fr.qml.ts
@@ -14,23 +14,23 @@ lupdate -locations absolute ../qml        -ts ../translations/sakura_fr.qml.ts
 lupdate -locations absolute ../sakura.pro -ts ../translations/sakura_it.src.ts
 lupdate -locations absolute ../qml        -ts ../translations/sakura_it.qml.ts
 
-lupdate -locations absolute ../sakura.pro -ts ../translations/sakura_es.src.ts
-lupdate -locations absolute ../qml        -ts ../translations/sakura_es.qml.ts
-
-lupdate -locations absolute ../sakura.pro -ts ../translations/sakura_zh.src.ts
-lupdate -locations absolute ../qml        -ts ../translations/sakura_zh.qml.ts
-
 lupdate -locations absolute ../sakura.pro -ts ../translations/sakura_ja.src.ts
 lupdate -locations absolute ../qml        -ts ../translations/sakura_ja.qml.ts
 
 lupdate -locations absolute ../sakura.pro -ts ../translations/sakura_ko.src.ts
 lupdate -locations absolute ../qml        -ts ../translations/sakura_ko.qml.ts
 
-lconvert ../translations/sakura_ru.src.ts ../translations/sakura_ru.qml.ts -o ../translations/sakura_ru.ts
+lupdate -locations absolute ../sakura.pro -ts ../translations/sakura_ru.src.ts
+lupdate -locations absolute ../qml        -ts ../translations/sakura_ru.qml.ts
+
+lupdate -locations absolute ../sakura.pro -ts ../translations/sakura_zh.src.ts
+lupdate -locations absolute ../qml        -ts ../translations/sakura_zh.qml.ts
+
 lconvert ../translations/sakura_de.src.ts ../translations/sakura_de.qml.ts -o ../translations/sakura_de.ts
+lconvert ../translations/sakura_es.src.ts ../translations/sakura_es.qml.ts -o ../translations/sakura_es.ts
 lconvert ../translations/sakura_fr.src.ts ../translations/sakura_fr.qml.ts -o ../translations/sakura_fr.ts
 lconvert ../translations/sakura_it.src.ts ../translations/sakura_it.qml.ts -o ../translations/sakura_it.ts
-lconvert ../translations/sakura_es.src.ts ../translations/sakura_es.qml.ts -o ../translations/sakura_es.ts
-lconvert ../translations/sakura_zh.src.ts ../translations/sakura_zh.qml.ts -o ../translations/sakura_zh.ts
 lconvert ../translations/sakura_ja.src.ts ../translations/sakura_ja.qml.ts -o ../translations/sakura_ja.ts
 lconvert ../translations/sakura_ko.src.ts ../translations/sakura_ko.qml.ts -o ../translations/sakura_ko.ts
+lconvert ../translations/sakura_ru.src.ts ../translations/sakura_ru.qml.ts -o ../translations/sakura_ru.ts
+lconvert ../translations/sakura_zh.src.ts ../translations/sakura_zh.qml.ts -o ../translations/sakura_zh.ts
