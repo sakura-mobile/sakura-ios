0b89783b34056acdc9f63152334c6de8f060533e
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2020-08-02 02:56:34 +0300

    Use autoloadEnabled = YES to reload ads. It looks like it should use original request with proper extras.

--- src/admobhelper.mm
+++ src/admobhelper.mm
@@ -15,7 +15,7 @@ const QString AdMobHelper::ADMOB_TEST_DEVICE_ID      (QLatin1String(""));
 
 namespace {
 
-constexpr NSTimeInterval AD_RELOAD_DELAY = 60.0;
+constexpr NSTimeInterval AD_RELOAD_ON_FAILURE_DELAY = 60.0;
 
 }
 
@@ -56,7 +56,7 @@ constexpr NSTimeInterval AD_RELOAD_DELAY = 60.0;
         BannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeSmartBannerPortrait];
 
         BannerView.adUnitID                                  = AdMobHelper::ADMOB_BANNERVIEW_UNIT_ID.toNSString();
-        BannerView.autoloadEnabled                           = NO;
+        BannerView.autoloadEnabled                           = YES;
         BannerView.translatesAutoresizingMaskIntoConstraints = NO;
         BannerView.rootViewController                        = root_view_controller;
         BannerView.delegate                                  = self;
@@ -129,8 +129,6 @@ constexpr NSTimeInterval AD_RELOAD_DELAY = 60.0;
     if (AdMobHelperInstance != nullptr) {
         AdMobHelperInstance->SetBannerViewHeight(qFloor(BannerView.frame.size.height + root_view_controller.view.safeAreaInsets.bottom));
     }
-
-    [self performSelector:@selector(loadAd) withObject:nil afterDelay:AD_RELOAD_DELAY];
 }
 
 - (void)adViewWillPresentScreen:(GADBannerView *)adView
@@ -154,7 +152,7 @@ constexpr NSTimeInterval AD_RELOAD_DELAY = 60.0;
 
     qWarning() << QString::fromNSString(error.localizedDescription);
 
-    [self performSelector:@selector(loadAd) withObject:nil afterDelay:AD_RELOAD_DELAY];
+    [self performSelector:@selector(loadAd) withObject:nil afterDelay:AD_RELOAD_ON_FAILURE_DELAY];
 }
 
 @end
@@ -306,7 +304,7 @@ constexpr NSTimeInterval AD_RELOAD_DELAY = 60.0;
 
     qWarning() << QString::fromNSString(error.localizedDescription);
 
-    [self performSelector:@selector(loadAd) withObject:nil afterDelay:AD_RELOAD_DELAY];
+    [self performSelector:@selector(loadAd) withObject:nil afterDelay:AD_RELOAD_ON_FAILURE_DELAY];
 }
 
 @end
